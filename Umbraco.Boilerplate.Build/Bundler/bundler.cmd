2454cc821c00bdcb4d58476c723c937ad867006f
Johan Halvarsson de Maar <johanhdm@gmail.com>
2015-01-20 13:03:18 +0100

    css fixes

--- Umbraco.Boilerplate.Build/Bundler/bundler.cmd
+++ Umbraco.Boilerplate.Build/Bundler/bundler.cmd
@@ -3,7 +3,7 @@ pushd "%~dp0"
 
 :: process Content and Scripts by default
 if "%*" == "" (
-    node bundler.js  ../../Umbraco.Boilerplate.Web/Static/Styles ../../Umbraco.Boilerplate.Web/Static/Scripts
+    node bundler.js  ../../Umbraco.Boilerplate.Web/Static/Styles ../../Umbraco.Boilerplate.Web/Static/Scripts #skipmin
 ) else (
     node bundler.js %*
 )
